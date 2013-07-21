ce2f0da593cb78a8d9f6e41a2209fd86ae3eaab2
Rosetta Training <rosetta.training@gmail.com>
2013-07-20 21:12:02 -0400

    Creating the Category Entity and the Category Controller

--- rosetta-demo-ui-businesslogic/src/main/java/com/rosetta/demo/components/categorie/controller/CategoryController.java
+++ rosetta-demo-ui-businesslogic/src/main/java/com/rosetta/demo/components/categorie/controller/CategoryController.java
@@ -1,5 +1,26 @@
 package com.rosetta.demo.components.categorie.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rosetta.demo.components.categorie.entity.Category;
+import com.rosetta.demo.components.generic.entity.GenericConfiguration;
+
 public class CategoryController {
+	
+	/**
+	 * Method to get the top categories.
+	 * 
+	 * http://localhost/wcs/resources/store/10152/categoryview/@top
+	 * 
+	 * @param configuration GenericConfiguration object
+	 * @return List with the top categories
+	 */
+	public List<Category> getTopCategories(GenericConfiguration configuration) {
+
+		List<Category> topCategories = new ArrayList<Category>();
+
+		return topCategories;
+	}
 
 }
--- rosetta-demo-ui-businesslogic/src/main/java/com/rosetta/demo/components/categorie/entity/Category.java
+++ rosetta-demo-ui-businesslogic/src/main/java/com/rosetta/demo/components/categorie/entity/Category.java
@@ -1,5 +1,119 @@
 package com.rosetta.demo.components.categorie.entity;
 
 public class Category {
+	
+	
+	private String identifier;
+	private String name;
+	private String productsURL;
+	private String resourceId;
+	private String shortDescription;
+	private String thumbnail;
+	private long uniqueID;
+	
+	/**
+	 * 
+	 */
+	public Category() {
+		super();
+	}
+
+	/**
+	 * @return the identifier
+	 */
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	/**
+	 * @param identifier the identifier to set
+	 */
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the productsURL
+	 */
+	public String getProductsURL() {
+		return productsURL;
+	}
+
+	/**
+	 * @param productsURL the productsURL to set
+	 */
+	public void setProductsURL(String productsURL) {
+		this.productsURL = productsURL;
+	}
+
+	/**
+	 * @return the resourceId
+	 */
+	public String getResourceId() {
+		return resourceId;
+	}
+
+	/**
+	 * @param resourceId the resourceId to set
+	 */
+	public void setResourceId(String resourceId) {
+		this.resourceId = resourceId;
+	}
+
+	/**
+	 * @return the shortDescription
+	 */
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	/**
+	 * @param shortDescription the shortDescription to set
+	 */
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	/**
+	 * @return the thumbnail
+	 */
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	/**
+	 * @param thumbnail the thumbnail to set
+	 */
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	/**
+	 * @return the uniqueID
+	 */
+	public long getUniqueID() {
+		return uniqueID;
+	}
+
+	/**
+	 * @param uniqueID the uniqueID to set
+	 */
+	public void setUniqueID(long uniqueID) {
+		this.uniqueID = uniqueID;
+	}	
 
 }
